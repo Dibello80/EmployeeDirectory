e8cc6df7fbb16dc51200bf8b800457ad397569a0
Dibello80 <angelodibellomusic@gmail.com>
2021-01-19 14:09:33 -0800

    src

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import User from "./components/User";
+
+function App() {
+    return (
+        <div> 
+            <Header />
+            <User />
+        </div>
+    )
+}
+
+export default App;
